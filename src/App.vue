69fbbf0fb12c54ea159d137885e97a5a8477646a
苏丽 <su_lihappy@126.com>
2017-11-17 14:37:50 +0800

    修改工作区而忘记创建分支

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,13 @@
 <template>
-    <div id="app" class="uk-container uk-container-center">
-        <h1 class="uk-text-center">极好的竞赛题</h1>
-        <hr>
-        <transition name="flip" mode="out-in">
-            <!--动态组件-->
-            <component
-                    :is="mode"
-                    @confirmed="mode='app-question'"
-                    @answered="answered($event)"
-            ></component>
-        </transition>
-        <hr>
-    <!--<app-user></app-user>-->
+    <div class="uk-container uk-container-center">
+        <routerview></routerview>
+        <!--<app-user></app-user>-->
         <app-add-quote></app-add-quote>
     </div>
 </template>
 
 <script>
-    import Question from './components/Question.vue';
-    import Answer from './components/Answer.vue';
+
     import User from './components/user.vue';
     import AddQuote from './components/AddQuote.vue';
 
@@ -29,22 +18,7 @@
                 mode: 'app-question'
             }
         },
-        methods: {
-            answered(event){
-                if(event){
-                    this.mode = 'app-answer'
-                }else{
-                    alert('wrong, try again!');
-                    this.mode = 'app-question'
-                }
-            }
-        },
-        components: {
-            appQuestion: Question,
-            appAnswer: Answer,
-            appUser: User,
-            appAddQuote: AddQuote
-        }
+
     }
 </script>
 
@@ -52,31 +26,37 @@
     .uk-panel {
         margin: 0 auto;
     }
-    .flip-enter{
+
+    .flip-enter {
 
     }
-    .flip-enter-active{
+
+    .flip-enter-active {
         animation: flip-in .5s ease-out forwards;
     }
-    .flip-leave{
+
+    .flip-leave {
 
     }
-    .flip-leave-active{
+
+    .flip-leave-active {
         animation: flip-out .5s ease-out forwards;
     }
+
     @keyframes flip-out {
-        from{
+        from {
             transform: rotateY(0deg);
         }
-        to{
+        to {
             transform: rotateY(90deg);
         }
     }
+
     @keyframes flip-in {
-        from{
+        from {
             transform: rotateY(90deg);
         }
-        to{
+        to {
             transform: rotateY(0deg);
         }
     }
---
+++ src/components/competition/Competition.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1 class="uk-text-center">极好的竞赛题</h1>
+        <hr>
+        <transition name="flip" mode="out-in">
+            <!--动态组件-->
+            <component
+                    :is="mode"
+                    @confirmed="mode='app-question'"
+                    @answered="answered($event)"
+            ></component>
+        </transition>
+        <hr>
+    </div>
+</template>
+<script>
+    import Question from './components/Question.vue';
+    import Answer from './components/Answer.vue';
+    export default{
+        data(){
+            return {
+                mode: 'app-question'
+            }
+        },
+        methods: {
+            answered(event){
+                if(event){
+                    this.mode = 'app-answer'
+                }else{
+                    alert('wrong, try again!');
+                    this.mode = 'app-question'
+                }
+            }
+        }
+    }
+</script>
